bc61a5816e58853f017c1fa35e842a8d0bbe4da6
Alireza Nazari <a.alirezanazari@gmail.com>
2021-03-25 21:26:50 +0430

    Implement empty MainActivity.kt

---
+++ app/src/main/java/ir/alirezanazari/clubhome/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.alirezanazari.clubhome.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.alirezanazari.clubhome.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
