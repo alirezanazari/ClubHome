e5ec43f7863db6e96913f91caa78b9e5657fd411
Alireza Nazari <a.alirezanazari@gmail.com>
2021-04-09 19:49:38 +0430

    Implement phone number verification step

--- app/src/main/java/ir/alirezanazari/clubhome/ui/login/LoginFragment.kt
+++ app/src/main/java/ir/alirezanazari/clubhome/ui/login/LoginFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import ir.alirezanazari.clubhome.R
 import ir.alirezanazari.clubhome.databinding.FragmentLoginBinding
 import ir.alirezanazari.clubhome.ui.base.BaseFragment
 import ir.alirezanazari.clubhome.util.*
+import ir.alirezanazari.domain.entity.register.CompletePhoneNumberAuth
 import javax.inject.Inject
 
 // Written by Alireza Nazari, <@ali_rezaNazari> <a.alirezaNazari@gmail.com>.
@@ -44,38 +46,91 @@ class LoginFragment :
                 }
             }
             actionButton.setOnClickListener {
-                val number = phoneEditText.text.toString()
-                if (number.isPhoneNumber()) {
-                    viewModel.login(countryPickerView.number)
-                }
+                onActionButtonClicked()
             }
         }
     }
 
     override fun doLogics() {
-        onBackPressed {
-            requireActivity().finish()
-        }
         observeError()
         observerLoading()
         observeLogin()
-    }
-
-    private fun observeLogin() {
-        viewModel.loginResponse.observe(viewLifecycleOwner) { response ->
-            log(response.toString())
+        observeVerification()
+        onBackPressed {
+            requireActivity().finish()
         }
     }
 
-    private fun observeError(){
+    private fun observeLogin() {
+        viewModel.loginResponse.observe(viewLifecycleOwner) {
+            binding.apply {
+                codeEditText.apply {
+                    visible()
+                    requestFocus()
+                }
+                actionButton.setText(R.string.verify_code)
+            }
+        }
+    }
+
+    private fun observeVerification() {
+        viewModel.verifyResponse.observe(viewLifecycleOwner) { response ->
+            navigateToNextStep(response)
+        }
+    }
+
+    private fun observeError() {
         viewModel.errorMessage.observe(viewLifecycleOwner) {
             toast(it.message)
+            binding.actionButton.setText(getActionButtonText())
         }
     }
 
     private fun observerLoading() {
         viewModel.loading.observe(viewLifecycleOwner) { isLoading ->
-            binding.actionButton.text = getString(if (isLoading) R.string.loading else R.string.next)
+            binding.actionButton.setText(
+                if (isLoading) R.string.loading else getActionButtonText()
+            )
         }
     }
+
+    private fun navigateToNextStep(response: CompletePhoneNumberAuth.Response) {
+        findNavController().navigate(
+            when {
+                response.isWaitlisted ->
+                    LoginFragmentDirections.actionLoginToWaitingList()
+
+                response.userProfile?.username.isNullOrEmpty() ->
+                    LoginFragmentDirections.actionLoginToRegister()
+
+                else ->
+                    LoginFragmentDirections.actionLoginToMain()
+            }
+        )
+    }
+
+    private fun onActionButtonClicked() {
+        val number = binding.countryPickerView.number
+        if (!viewModel.isLoggedIn) {
+            login(number)
+        } else {
+            verify(number)
+        }
+    }
+
+    private fun login(number: String) {
+        if (number.isPhoneNumber()) {
+            viewModel.login(number)
+        } else {
+            toast(getString(R.string.phone_number_is_wrong))
+        }
+    }
+
+    private fun verify(number: String) {
+        val verifyCode = binding.codeEditText.text.toString()
+        viewModel.verify(number, verifyCode)
+    }
+
+    private fun getActionButtonText() =
+        if (viewModel.isLoggedIn) R.string.verify_code else R.string.next
 }
--- app/src/main/java/ir/alirezanazari/clubhome/ui/login/LoginViewModel.kt
+++ app/src/main/java/ir/alirezanazari/clubhome/ui/login/LoginViewModel.kt
@@ -1,26 +1,60 @@
 package ir.alirezanazari.clubhome.ui.login
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import ir.alirezanazari.clubhome.ui.base.BaseViewModel
+import ir.alirezanazari.clubhome.util.LiveEvent
 import ir.alirezanazari.clubhome.util.updateOnSuccess
+import ir.alirezanazari.data.util.SessionManager
+import ir.alirezanazari.domain.entity.base.ResultEntity
+import ir.alirezanazari.domain.entity.register.CompletePhoneNumberAuth
 import ir.alirezanazari.domain.entity.register.StartPhoneNumberAuth
 import ir.alirezanazari.domain.usecase.register.GetLoginUseCase
+import ir.alirezanazari.domain.usecase.register.GetVerifyUseCase
 import javax.inject.Inject
 
 // Written by Alireza Nazari, <@ali_rezaNazari> <a.alirezaNazari@gmail.com>.
 
 class LoginViewModel @Inject constructor(
-    private val loginUseCase: GetLoginUseCase
-): BaseViewModel() {
+    private val loginUseCase: GetLoginUseCase,
+    private val verifyUseCase: GetVerifyUseCase,
+    private val sessionManager: SessionManager
+) : BaseViewModel() {
 
-    private val _loginResponse =  MutableLiveData<StartPhoneNumberAuth.Response>()
-    val loginResponse : LiveData<StartPhoneNumberAuth.Response>
+    var isLoggedIn = false
+
+    private val _loginResponse = LiveEvent<StartPhoneNumberAuth.Response>()
+    val loginResponse: LiveData<StartPhoneNumberAuth.Response>
         get() = _loginResponse
 
     fun login(number: String) = launchRequest {
         loginUseCase.invoke(StartPhoneNumberAuth.Request(number))
             .updateOnSuccess(_loginResponse)
+            .also {
+                if (it.isSuccessful()) isLoggedIn = true
+            }
     }
 
+
+    private val _verifyResponse = LiveEvent<CompletePhoneNumberAuth.Response>()
+    val verifyResponse: LiveData<CompletePhoneNumberAuth.Response>
+        get() = _verifyResponse
+
+    fun verify(phoneNumber: String, code: String) = launchRequest {
+        verifyUseCase.invoke(CompletePhoneNumberAuth.Request(phoneNumber, code))
+            .updateOnSuccess(_verifyResponse)
+            .also { result ->
+                if (result.isSuccessful())
+                    saveUserProfile((result as ResultEntity.Success).data)
+            }
+    }
+
+
+    private fun saveUserProfile(response: CompletePhoneNumberAuth.Response) {
+        sessionManager.apply {
+            userToken = response.authToken
+            userID = response.userProfile?.userId.toString()
+            isWaitlisted = response.isWaitlisted
+            write()
+        }
+    }
 }
