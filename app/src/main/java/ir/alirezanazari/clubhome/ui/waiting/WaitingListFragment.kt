0d03f20b44c6a97ec12f5d82772e5f4582f65406
Alireza Nazari <a.alirezanazari@gmail.com>
2021-04-13 19:26:11 +0430

    Implement WaitingListFragment.kt

--- app/src/main/java/ir/alirezanazari/clubhome/ui/waiting/WaitingListFragment.kt
+++ app/src/main/java/ir/alirezanazari/clubhome/ui/waiting/WaitingListFragment.kt
@@ -1,12 +1,18 @@
 package ir.alirezanazari.clubhome.ui.waiting
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
+import androidx.core.text.underline
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import ir.alirezanazari.clubhome.R
 import ir.alirezanazari.clubhome.databinding.FragmentWaitingListBinding
 import ir.alirezanazari.clubhome.ui.base.BaseFragment
 import ir.alirezanazari.clubhome.util.ViewModelFactory
+import ir.alirezanazari.clubhome.util.getColor
 import javax.inject.Inject
 
 // Written by Alireza Nazari, <@ali_rezaNazari> <a.alirezaNazari@gmail.com>.
@@ -30,4 +36,17 @@ class WaitingListFragment :
         return FragmentWaitingListBinding.inflate(inflater, container, false)
     }
 
+    override fun bindViews(savedInstanceState: Bundle?) {
+        binding.logoutButton.apply {
+            text = buildSpannedString {
+                color(getColor(R.color.primary)) {
+                    underline { append(getString(R.string.logout)) }
+                }
+            }
+            setOnClickListener {
+                viewModel.logout()
+                findNavController().popBackStack()
+            }
+        }
+    }
 }
--- app/src/main/java/ir/alirezanazari/clubhome/ui/waiting/WaitingListViewModel.kt
+++ app/src/main/java/ir/alirezanazari/clubhome/ui/waiting/WaitingListViewModel.kt
@@ -1,9 +1,16 @@
 package ir.alirezanazari.clubhome.ui.waiting
 
 import ir.alirezanazari.clubhome.ui.base.BaseViewModel
+import ir.alirezanazari.data.util.SessionManager
 import javax.inject.Inject
 
 // Written by Alireza Nazari, <@ali_rezaNazari> <a.alirezaNazari@gmail.com>.
 
-class WaitingListViewModel @Inject constructor(): BaseViewModel() {
+class WaitingListViewModel @Inject constructor(
+    private val sessionManager: SessionManager
+) : BaseViewModel() {
+
+    fun logout() {
+        sessionManager.logout()
+    }
 }
